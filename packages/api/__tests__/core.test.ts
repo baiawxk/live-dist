39eb6f5e3efdf0c1501e6927bee9229006c5ddbe
baiawxk <baiawxk@qq.com>
2025-09-27 21:10:36 +0800

    upd lint:fix 范围和代码风格

--- packages/api/__tests__/core.test.ts
+++ packages/api/__tests__/core.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { z } from 'zod'
 import { createApiSchema } from '../src/core'
 
--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,13 +1,13 @@
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
